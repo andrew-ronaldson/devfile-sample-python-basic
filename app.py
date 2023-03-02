514404774dba29a769c31fb7e47188aea1954178
Andrew Ronaldson <aronalds@redhat.com>
2023-03-02 14:21:05 -0400

    New greeting from a cat

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return "Hello World, it's your friend Mr.Whiskers!"
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
