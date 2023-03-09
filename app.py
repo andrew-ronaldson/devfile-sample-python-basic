2354b92574534995fc9d17da86a59464b5684410
Andrew Ronaldson <aronalds@redhat.com>
2023-03-09 14:11:33 -0400

    Tom is here

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World, it's your friend Mr.Belvedere!"
+    return "Hello World, it's your friend Mr.Bombadil!"
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
